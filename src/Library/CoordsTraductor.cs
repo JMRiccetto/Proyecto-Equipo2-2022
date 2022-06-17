d35234034fab9aaf8958c4953c1c825b253f0f1d
Franco Robotti <FRANCO.ROBOTTI@correo.ucu.edu.uy>
2022-06-16 21:21:35 -0300

    Traductor de coordenadas

--- src/Library/CoordsTraductor.cs
+++ src/Library/CoordsTraductor.cs
@@ -1,3 +1,6 @@
+using System;
+
+
 namespace NavalBattle
 {
     public class  CoordsTranslate
@@ -7,89 +10,19 @@ namespace NavalBattle
         private string CoordsY { get; set; }
 
         //toma como entrada un mensaje del usuario y lo traduce a coordenadas del tablero.
-        public string Translate(string aCoordsLocation)
+        public string Translate(string aCoordsLocation) //Juan, cambia esto porque no se cual es la variante de la coordenada.
         {
-            string Coords = aCoordsLocation; // A1
+            string Coords = aCoordsLocation; // A1 //Juan, cambia esto porque no se cual es la variante de la coordenada.
+
             CoordsX = Coords[0].ToString(); // A
             CoordsY = Coords[1].ToString(); // 1
 
-            TranslateX(CoordsX);
-            TranslateY(CoordsY);
+            CoordsX = (char.Parse(CoordsX) - 65).ToString();
+            CoordsY = (Int32.Parse(CoordsY) - 1).ToString();
 
-            string translatedCoords =  (TranslateX + TranslateY); // 11
+            string translatedCoords =  (CoordsX + CoordsY); // 11
             
             return translatedCoords;
-        }
-
-        public string TranslateX(string CoordsX)
-        {
-            if (CoordsX == "A")
-            {
-                return "0";
-            }
-            else if (CoordsX == "B")
-            {
-                return "1";
-            }
-            else if (CoordsX == "C")
-            {
-                return "2";
-            }
-            else if (CoordsX == "D")
-            {
-                return "3";
-            }
-            else if (CoordsX == "E")
-            {
-                return "4";
-            }
-            else if (CoordsX == "F")
-            {
-                return "5";
-            }
-            else if (CoordsX == "G")
-            {
-                return "6";
-            }
-            else
-            {
-                return "7";
-            }
-        }        
-        public string TranslateY(string CoordsY)
-        {
-            if (CoordsY == "1")
-            {
-                return "0";
-            }
-            else if (CoordsY == "2")
-            {
-                return "1";
-            }
-            else if (CoordsY == "3")
-            {
-                return "2";
-            }
-            else if (CoordsY == "4")
-            {
-                return "3";
-            }
-            else if (CoordsY == "5")
-            {
-                return "4";
-            }
-            else if (CoordsY == "6")
-            {
-                return "5";
-            }
-            else if (CoordsY == "7")
-            {
-                return "6";
-            }
-            else
-            {
-                return "7";
-            }
         }
     }
 }
