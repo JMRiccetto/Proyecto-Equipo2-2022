8ddca2756055a1f23bf029a37aa18a4fbe11412f
Franco Robotti <FRANCO.ROBOTTI@correo.ucu.edu.uy>
2022-07-07 20:51:33 -0300

    defensa

--- test/LibraryTests/ShotTest.cs
+++ test/LibraryTests/ShotTest.cs
@@ -14,17 +14,36 @@ namespace Test.Library
         [SetUp]
         public void Setup()
         {
-            gameboard = new Gameboard(6);        }
+            gameboard = new Gameboard(6);        
+        }
 
         [Test]
-        public void shotIs0()
+        public void shotWaterIs0()
         {
             Assert.AreEqual(gameboard.Xcount, 0);
         }
 
 
         [Test]
-        public void AttackGameboardPrinter()
+        public void shotTakeIs0()
+        {
+            NavalBattle.Coords coords = new Coords("11");
+            gameboard.AddShip(3, "00", "S");
+            gameboard.RecieveAttack(coords);
+            Assert.AreEqual(gameboard.Tcount, 0);
+        }
+
+        [Test]
+        public void shotWaterIs01()
+        {
+            NavalBattle.Coords coords = new Coords("44");
+            gameboard.AddShip(2, "11", "S");
+            gameboard.RecieveAttack(coords);
+            Assert.AreEqual(gameboard.Xcount, 1);
+        }
+
+        [Test]
+        public void shotTakeIs1()
         {
             NavalBattle.Coords coords = new Coords("00");
             gameboard.AddShip(3, "00", "S");
