97ea14afc5769ce1ea825948adc454301494f258
Franco Robotti <FRANCO.ROBOTTI@correo.ucu.edu.uy>
2022-07-07 18:23:08 -0300

    rama defensa

--- src/Library/Serializer/MyReferenceHandler.cs
+++ src/Library/Serializer/MyReferenceHandler.cs
@@ -3,7 +3,7 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace NavalBattle
-{
+{ // hola
     public class MyReferenceHandler : ReferenceHandler
     {
         private static MyReferenceHandler instance;
